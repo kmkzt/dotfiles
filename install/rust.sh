ad9b1756d73bc89eac5f60969dfdc105807ea1c3
kmkzt <info.pscreator@gmail.com>
2019-07-05 13:41:09 +0900

    feat: add cargo-edit

--- install/rust.sh
+++ install/rust.sh
@@ -2,3 +2,13 @@ if test ! $(which rustc); then
   echo "Installing rust..."
   curl https://sh.rustup.rs -sSf | sh
 fi
+
+# Install Cargo modules
+# cargo-edit
+# https://github.com/killercup/cargo-edit
+mod=(
+  cargo-edit 
+)
+
+echo "installing node modules..."
+cargo install ${mod[@]}
