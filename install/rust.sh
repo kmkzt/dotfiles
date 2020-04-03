03d1d57f16237eebc937fbfae8d6feab4ad8854f
kmkzt <info.pscreator@gmail.com>
2020-04-03 23:53:53 +0900

    feat: add rust-tools

--- install/rust.sh
+++ install/rust.sh
@@ -3,6 +3,14 @@ if test ! $(which rustc); then
   curl https://sh.rustup.rs -sSf | sh
 fi
 
+# update version
+rustup update
+
+# language server: https://github.com/rust-lang/rls
+# formatter:  https://github.com/rust-lang/rust-clippy
+rustup component add rls rust-analysis rust-src rust-fmt clippy
+rustup toolchain add nightly
+
 # Cargo modules
 # cargo-edit: https://github.com/killercup/cargo-edit
 # cargo-generate: https://github.com/ashleygwilliams/cargo-generate
@@ -16,9 +24,5 @@ mod=(
 echo "installing cargo modules..."
 cargo install ${mod[@]}
 
-# update version
-rustup update
-
-# language server: https://github.com/rust-lang/rls
-# formatter:  https://github.com/rust-lang/rust-clippy
-rustup component add rls rust-analysis rust-src rust-fmt clippy
+# nightly install
+cargo +nightly install racer
