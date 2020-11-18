f712bc2b450d21ac5ef7e501d62774d3b9bca717
kmkzt <info.pscreator@gmail.com>
2020-11-19 00:08:23 +0900

    chore: add .gitconfig

--- link.sh
+++ link.sh
@@ -11,5 +11,8 @@ ln -sf $DOTFILES_PATH/config/.zshrc ~/.zshrc
 ln -sf $DOTFILES_PATH/config/.vimrc ~/.vimrc
 ln -sf $DOTFILES_PATH/config/nvim/init.vim ~/.config/nvim/init.vim
 
+# git copy default config
+cat $DOTFILES_PATH/config/.gitconfig > ~/.gitconfig
+
 # commitizen: https://github.com/commitizen/cz-cli
 ln -sf $DOTFILES_PATH/config/.czrc ~/.czrc
