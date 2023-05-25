b7cbc5aa65987d8799420de2d8a39b5dacaae5b5
kmkzt <info.pscreator@gmail.com>
2023-05-25 15:28:12 +0900

    fix: fix vim install scripts

--- scripts/install/vim.sh
+++ scripts/install/vim.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 # https://github.com/junegunn/vim-plug/wiki/tips#automatic-installation
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # nvim-budle: https://github.com/Shougo/dein.vim
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-# For example, we just use `~/.cache/dein` as installation directory
-sh ./installer.sh ~/.cache/dein
-rm ./installer.sh
+DEIN_INSTALLER=$(dirname $0)/installer.sh
+curl -fsSL https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh > $DEIN_INSTALLER
+sh ./$DEIN_INSTALLER ~/.cache/dein
+rm ./$DEIN_INSTALLER
 
 # https://github.com/Shougo/deoplete.nvim
 pip3 install --user pynvim
