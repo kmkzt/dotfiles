aa7284fc9c346dbaa0ff1a0878e1ceec519087ce
kmkzt <info.pscreator@gmail.com>
2024-07-01 13:43:12 +0900

    fix java setup

--- scripts/install/java.sh
+++ scripts/install/java.sh
@@ -1,2 +1,4 @@
-echo "insalling java..."
+echo "insalling java...";
+
 brew install openjdk@17
+sudo ln -sfn /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
