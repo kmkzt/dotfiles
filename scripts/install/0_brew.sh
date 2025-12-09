1283962ac2971a8e42f2d63b8208db447649cbd4
kmkzt <info.pscreator@gmail.com>
2025-12-09 10:28:16 +0900

    fix

--- scripts/install/0_brew.sh
+++ scripts/install/0_brew.sh
@@ -2,7 +2,7 @@
 
 if test ! $(which brew); then
 	echo "Installing homebrew..."
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 else
 	echo "Homebrew already installed"
 fi
--- scripts/install/node.sh
+++ scripts/install/node.sh
@@ -1,30 +1,25 @@
 #!/bin/sh
 
-NODE_VERSION="20.15.0"
-CURRENT_NODE_VERSION=$(node -v | sed -e 's/v//')
+NODE_VERSION="24.11.1"
 
 setup_nodenv() {
-	eval "$(anyenv install --init)";
-	eval "$(anyenv install nodenv)";
-	eval "$(nodenv init -)";
-	eval "$(~/.anyenv/envs/nodenv/plugins/node-build/install.sh)";
+	if test ! $(which nodenv); then
+		eval "$(anyenv install --init)"
+		eval "$(anyenv install nodenv)"
+		eval "$(nodenv init -)"
+		eval "$(~/.anyenv/envs/nodenv/plugins/node-build/install.sh)"
+	else
+		echo "nodenv already installed."
+	fi
 }
 
 setup_node() {
-	eval "$(nodenv install $NODE_VERSION)";
-	eval "$(nodenv global $NODE_VERSION)";
-	eval "$(nodenv rehash)";
-	corepack enable pnpm;
+	eval "$(nodenv install $NODE_VERSION -f)"
+	eval "$(nodenv global $NODE_VERSION)"
+	eval "$(nodenv rehash)"
 }
 
-if test ! $(which node); then
-	echo "installing nodenv...";
-	setup_nodenv;
-	echo "installing node...";
-	setup_node;
-elif [ "$1" = "-u" ] || [ $NODE_VERSION != $CURRENT_NODE_VERSION ]; then
-	echo "Update node...";
-	setup_node;
-else
-	echo "nodenv already installed";
-fi
+echo "Setup nodenv..."
+setup_nodenv;
+echo "Setup node..."
+setup_node;
